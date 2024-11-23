ba9dad66b02d20be5458badeee5a5b9c1047c01c
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-22 22:30:32 -0500

    Gas information lookup

--- client/src/crypto_hooks/gas/useFetchGasLookup.ts
+++ client/src/crypto_hooks/gas/useFetchGasLookup.ts
@@ -1,7 +1,17 @@
-import axios from "axios";
 import { useFetch } from "../../custom_hooks/useFetch";
 
-// Fetch ETH, Layer Two Gas Pricing
+// Fetching Address to ENS Custom hook
 export const useFetchGasLookup = () => {
+    const URL = 'http://localhost:5000/gas-information'; // Define the API endpoint
     
+    const options = {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    };
+
+    const state = useFetch(URL, options); // Use the custom hook to fetch data
+
+    return state; // Return the state from the custom hook
 }
