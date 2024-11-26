b85afc9a7a8aadb4ca4dbd10c50158d3a84ede95
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-26 00:32:26 -0500

    ERC20 Holdings client hook codebase complete

---
+++ client/src/crypto_hooks/erc720Tokens/useFetchERC20Holdings.ts
@@ -0,0 +1,20 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC20 Collection Transfers
+export const useFetchERC20CollectionTransfers = async (contractAddress: string) => {
+
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000/erc20-collection-holdings', options);
+
+    // Return the state from the custom hook
+    return state;
+}
