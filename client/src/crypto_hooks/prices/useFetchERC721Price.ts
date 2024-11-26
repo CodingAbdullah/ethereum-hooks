c3c827e29e593b94fcb5e1b2099621f5d15814e3
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-25 23:22:05 -0500

    ERC721 Token Price client hook codebase complete

---
+++ client/src/crypto_hooks/prices/useFetchERC721Price.ts
@@ -0,0 +1,19 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC721 Prices Hook
+export const useFetchERC721Price = async (contractAddress: string, tokenID: number) => {
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress, tokenID }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000/erc721-token-price', options);
+
+    // Return the state from the custom hook
+    return state;
+}
