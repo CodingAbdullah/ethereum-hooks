06d3166ec4ec78c08c82ff136dfe61869ca41817
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-26 23:03:59 -0500

    ERC721 Collection Market cap client hook codebase complete

---
+++ client/src/crypto_hooks/erc721Tokens/useFetchERC721CollectionMarketCap.ts
@@ -0,0 +1,20 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC721 Collection Market Cap Hook
+export const useFetchERC721CollectionMarketCap = async (contractAddress: string, duration: 2 | 14 | 30) => {
+
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress, duration }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000/erc721-collection-market-cap', options);
+
+    // Return the state from the custom hook
+    return state;
+}
