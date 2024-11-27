12d793b47a32591271cb371766b57ed6d1c57fa4
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-26 23:13:42 -0500

    ERC721 Collection Sales client hook codebase complete

---
+++ client/src/crypto_hooks/erc721Tokens/useFetchERC721CollectionSales.ts
@@ -0,0 +1,20 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC721 Collection Sales Hook
+export const useFetchERC721CollectionSales = async (contractAddress: string) => {
+
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000/erc721-collection-sales', options);
+
+    // Return the state from the custom hook
+    return state;
+}
