0c1db6b0fc6bfd2a4e3d2b67cf001a9191b39bfb
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-27 00:23:56 -0500

    ERC721 Opensea client hook codebase complete

---
+++ client/src/crypto_hooks/erc721Tokens/useFetchERC721OpenseaData.ts
@@ -0,0 +1,20 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC721 Opensea data hook
+export const useFetchERC721OpenseaData = async (contractAddress: string, tokenID: string) => {
+    
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress, tokenID }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000/erc721-opensea-data', options);
+
+    // Return the state from the custom hook
+    return state;
+}
