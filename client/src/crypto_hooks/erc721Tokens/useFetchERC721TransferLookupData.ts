57868b510969c2d163d0a3c6fd596472522bbecb
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-27 00:48:47 -0500

    ERC721 Token Transfer Lookup data client hook codebase complete

---
+++ client/src/crypto_hooks/erc721Tokens/useFetchERC721TransferLookupData.ts
@@ -0,0 +1,20 @@
+import { useFetch } from "../../custom_hooks/useFetch";
+
+// Fetching ERC721 Transfer Lookup data hook
+export const useFetchERC721TransferLookupData = async (contractAddress: string, tokenID: string) => {
+    
+    // Set options for request
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({ contractAddress, tokenID }),
+        headers : {
+            'accept' : 'application/json'
+        }
+    }
+
+    // Use the custom hook to fetch data
+    const state = useFetch('http://localhost:5000//erc721-token-transfer-lookup-data', options);
+
+    // Return the state from the custom hook
+    return state;
+}
