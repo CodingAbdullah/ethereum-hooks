c664a4387f3b21142bc17f4bf00e570d7617e03e
CodingAbdullah <abdullahmd326@gmail.com>
2024-12-18 21:04:37 -0500

    Server side codebase modified for production, bugs fixed

--- server/server.ts
+++ server/server.ts
@@ -3,26 +3,52 @@ import express from "express";
 import cors, { CorsOptions } from 'cors'; // Import CorsOptions
 
 // Route files for working with different ETH protocols
-const addressENSRouter = require("./Routes/AddressENSRouter");
-const CoinPricesRouter = require("./Routes/CoinPriceRouter");
-const ENSTransferRouter = require("./Routes/ENSTransferRouter");
-const ERC720CollectionRouter = require("./Routes/ERC720CollectionRouter");
-const ERC720HoldingsRouter = require("./Routes/EsRC720HoldingsRouter");
-const ERC721CollectionRouter = require('./Routes/ERC721CollectionRouter');
-const ERC721LookupRouter = require("./Routes/ERC721LookupRouter");
-const ERC721HoldingsRouter = require('./Routes/ERC721HoldingsRouter');
-const GasTrackerRouter = require('./Routes/GasTrackerRouter');
-const GlobalDefiDataRouter = require("./Routes/GlobalDefiDataRouter");
-const MarketDataRouter = require('./Routes/MarketDataRouter');
-const TransactionsRouter = require('./Routes/TransactionsRouter');
-const WalletStatsRouter = require('./Routes/WalletStatsRouter');
+// ENS Routes
+const AddressToENSRouter = require("./Routes/ENSRoutes/AddressToENSRoute");
+const ENSIDRouter = require("./Routes/ENSRoutes/ENSIDRoute");
+const ENSNameRouter = require("./Routes/ENSRoutes/ENSNameRoute");
+const ENSToAddressRouter = require("./Routes/ENSRoutes/ENSToAddressRoute");
+
+// ERC20 Token Routes
+const ERC20CollectionOwnersRouter = require("./Routes/ERC20TokensRoutes/ERC20CollectionOwnersRoute");
+const ERC20CollectionTopCoinsRouter = require("./Routes/ERC20TokensRoutes/ERC20CollectionTopCoinsRoute");
+const ERC20CollectionTransfersRouter = require("./Routes/ERC20TokensRoutes/ERC20CollectionTransfersRoute");
+const ERC20HoldingsRouter = require("./Routes/ERC20TokensRoutes/ERC20HoldingsRoute");
+const ERC20TransfersRouter = require("./Routes/ERC20TokensRoutes/ERC20TransfersRoute");
+
+// ERC721 Token Routes
+const ERC721CollectionAttributesRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionAttributesRoute");
+const ERC721CollectionDataRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionDataRoute");
+const ERC721CollectionExtraDataRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionExtraDataRoute");
+const ERC721CollectionFloorPriceRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionFloorPriceRoute");
+const ERC721CollectionMarketCapRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionMarketCapRoute");
+const ERC721CollectionSalesRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionSalesRoute");
+const ERC721CollectionTransfersRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionTransfersRoute");
+const ERC721CollectionTrendsRouter = require('./Routes/ERC721TokensRoutes/ERC721CollectionTrendsRoute');
+const ERC721CollectionVolumeRouter = require("./Routes/ERC721TokensRoutes/ERC721CollectionVolumeRoute");
+const ERC721HoldingsRouter = require("./Routes/ERC721TokensRoutes/ERC721HoldingsRoute");
+const ERC721LookupDataRouter = require('./Routes/ERC721TokensRoutes/ERC721LookupDataRoute');
+const ERC721OpenseaDataRouter = require('./Routes/ERC721TokensRoutes/ERC721OpenseaDataRoute');
+const ERC721RarityDataRouter = require("./Routes/ERC721TokensRoutes/ERC721RarityDataRoute");
+const ERC721SalesDataRouter = require("./Routes/ERC721TokensRoutes/ERC721SalesDataRoute");
+const ERC721TransferLookupDataRouter = require("./Routes/ERC721TokensRoutes/ERC721TransferLookupDataRoute");
+const ERC721TransfersDataRouter = require("./Routes/ERC721TokensRoutes/ERC721TransfersDataRoute");
+
+// Gas Information Route
+const GasLookupRouter = require("./Routes/GasRoutes/GasLookupRoute");
+
+// Pricing Routes
+const ERC20PriceRouter = require("./Routes/PriceRoutes/ERC20PriceRoute");
+const ERC721PriceRouter = require('./Routes/PriceRoutes/ERC721PriceRoute');
+const ETHPriceRouter = require("./Routes/PriceRoutes/ETHPriceRoute");
+const LayerTwoPriceRouter = require("./Routes/PriceRoutes/LayerTwoPriceRoute");
 
 dotenv.config({ path: '.env' });
 const app = express();
 
 // CORS options to allow connections from client port
 const corsOptions: CorsOptions = {
-    origin: 'http://localhost:' + process.env.CLIENT_PORT // Allow requests from this origin
+    origin: process.env.CLIENT_URL // Allow requests from this origin
 };
 
 // Set up middleware
@@ -34,20 +60,37 @@ app.listen(process.env.PORT || 8080, () => {
     console.log("Listening on PORT " + (process.env.PORT || 8080));
 });
 
-// Enable API resources to server
-app.use("/", addressENSRouter);
-app.use("/", CoinPricesRouter);
-app.use("/", ENSTransferRouter);
-app.use("/", ERC720CollectionRouter);
-app.use("/", ERC720HoldingsRouter);
-app.use("/", ERC721CollectionRouter);
-app.use("/", ERC721LookupRouter);
+// Enable and activate routes to be used by server
+app.use("/", AddressToENSRouter);
+app.use("/", ENSIDRouter);
+app.use("/", ENSNameRouter);
+app.use("/", ENSToAddressRouter);
+app.use("/", ERC20CollectionOwnersRouter);
+app.use("/", ERC20CollectionTopCoinsRouter);
+app.use("/", ERC20CollectionTransfersRouter);
+app.use("/", ERC20HoldingsRouter);
+app.use("/", ERC20TransfersRouter);
+app.use("/", ERC721CollectionAttributesRouter);
+app.use("/", ERC721CollectionDataRouter);
+app.use("/", ERC721CollectionExtraDataRouter);
+app.use("/", ERC721CollectionFloorPriceRouter);
+app.use("/", ERC721CollectionMarketCapRouter);
+app.use("/", ERC721CollectionSalesRouter);
+app.use("/", ERC721CollectionTransfersRouter);
+app.use("/", ERC721CollectionTrendsRouter);
+app.use("/", ERC721CollectionVolumeRouter);
 app.use("/", ERC721HoldingsRouter);
-app.use("/", GasTrackerRouter);
-app.use("/", GlobalDefiDataRouter);
-app.use("/", MarketDataRouter);
-app.use("/", TransactionsRouter);
-app.use("/", WalletStatsRouter);
+app.use("/", ERC721LookupDataRouter);
+app.use("/", ERC721OpenseaDataRouter);
+app.use("/", ERC721RarityDataRouter);
+app.use("/", ERC721SalesDataRouter);
+app.use("/", ERC721TransferLookupDataRouter);
+app.use("/", ERC721TransfersDataRouter);
+app.use("/", GasLookupRouter);
+app.use("/", ERC20PriceRouter);
+app.use("/", ERC721PriceRouter);
+app.use("/", ETHPriceRouter);
+app.use("/", LayerTwoPriceRouter);
 
 // Export ready-made server for usage
 export default app;
